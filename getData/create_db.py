0a53ffd49047fb6346caaa7670059c2d93193003
John Zhang <johnz0923@gmail.com>
2018-11-12 19:51:16 +0000

    code to create the database used

---
+++ getData/create_db.py
@@ -0,0 +1,34 @@
+import MySQLdb
+
+class CreateDB:
+
+    def create_db(self, db_name, db_password):
+        db = MySQLdb.connect(host="localhost",
+                             user="root",
+                             passwd=db_password)
+        cur = db.cursor()
+
+        cur.execute("CREATE DATABASE github")
+
+        db = MySQLdb.connect(host="localhost",
+                             user="root",
+                             passwd=db_password,
+                             database=db_name)
+        cur = db.cursor()
+
+        cur.execute("CREATE TABLE repositories ("
+                    "id INT UNSIGNED NOT NULL,"
+                    "name VARCHAR(250) NOT NULL, "
+                    "stars INT UNSIGNED NOT NULL, "
+                    "forks INT UNSIGNED NOT NULL,"
+                    "contributors JSON,"
+                    "PRIMARY KEY (id))")
+
+        cur.execute("CREATE TABLE contributor ("
+                    "id INT UNSIGNED NOT NULL,"
+                    "login VARCHAR(250) NOT NULL, "
+                    "contribution INT UNSIGNED NOT NULL, "
+                    "location VARCHAR(250) NOT NULL,"
+                    "company VARCHAR(250) NOT NULL,"
+                    "PRIMARY KEY (id))")
+
